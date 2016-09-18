542343a25c6bfcc9aa588dbe9f430b6610005299
assassin3012 <assassin3012@mail.ru>
2016-09-19 00:32:10 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -2,16 +2,36 @@
 #include "stack.h"
 #include <iostream>
 #include <catch.hpp>
-
-SCENARIO("Stack", "[all]") 
-{
-	stack <bool> b;
-	REQUIRE( b.count() == 0);
-	stack <int> a;
-	a.push(1);
-	a.push(2);
-	a.push(3);
-	REQUIRE( a.pop() == 3);
-	REQUIRE( a.count() == 2);
-	REQUIRE( a.pop() == 2);
+SCENARIO("Stack init and count", "[init_count]"){
+	stack<bool> A;
+	REQUIRE(A.count() == 0);
+}
+SCENARIO("Stack push, top and pop", "[PTP]") {
+	stack<int> A;
+	A.push(4);
+	REQUIRE(A.count() == 1);
+	REQUIRE(A.top() == 4);
+	A.pop();
+	REQUIRE(A.count() == 0);
+}
+SCENARIO("operator =","[=]"){
+	stack<int> A;
+	A.push(221);
+	stack<int> B = A; 
+	REQUIRE(B.count() == 1);
+	REQUIRE(B.top() == 221);
+}
+SCENARIO("Stack push, top and pop #2", "[PTP#2]") {
+	stack<int> A;
+	int m=404;
+	for (int i=0, i<10, ++i){
+		A.push(m+i);
+	}
+	bool A=True;
+	for (int i=0, i<10, ++i){
+		if (A.count()!=10-i) { A=False;}
+		if (A.top!=m-i) { A=False; }
+		A.pop;
+	}
+	REQUIRE(A==True);
 }
