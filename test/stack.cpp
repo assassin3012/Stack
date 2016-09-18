d73ea3d14e11107cd939128730574a35e293c457
assassin3012 <assassin3012@mail.ru>
2016-09-19 00:09:33 +0300

    Update stack.cpp

--- test/stack.cpp
+++ test/stack.cpp
@@ -10,9 +10,7 @@ template <typename T>
 stack<T>::~stack() { delete[] array_; }
 
 template <typename T>
-stack<T>::stack(const stack<T> & st) : array_size_(st.array_size_), array_(new T[st.array_size_]) {
-	if (count > 0) { std::copy(st.array_, st.array_ + st.array_size_, array_); }
-}
+stack<T>::stack(const stack<T> & st) : array_size_(st.array_size_), count_(newcopy(st.array_, st.count_, st.array_size_);) {}
 template <typename T>
 size_t stack<T>::count() const noexcept { return count_; }
 template <typename T>
@@ -20,8 +18,7 @@ void stack<T>::push(T const & el) {
 	if (array_size_ == count_) {
 		if (array_size_ == 0) { array_size_ = 1; }
 		array_size_ = array_size_ * 2;
-		T * ar = new T[array_size_];
-		std::copy(array_, array_+count_, ar);
+		T * ar = newcopy(array_, count_, array_size_);
 		delete[] array_;
 		array_ = ar;
 	}
@@ -53,6 +50,14 @@ void stack<T>::swap(stack & st) {
 	std::swap(array_size_, st.array_size_);
 	std::swap(count_, st.count_);
 	std::swap(array_, st.array_);
+}
+template <typename T>
+T* stack<T>::newcopy(const T* st, const size_t new_count, const size_t new_size) {
+	T* ar = new T[new_size];
+	if (new_count > 0) {
+		std::copy(st, st + new_count, ar);
+	}
+	return ar;
 }
 #endif
  
