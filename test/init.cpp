99bd1f24d23ea30061e98eb70ca7cbdb162a2c6f
assassin3012 <assassin3012@mail.ru>
2016-09-19 00:36:49 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -9,6 +9,10 @@ SCENARIO("Stack init and count", "[init_count]"){
 SCENARIO("Stack push, top and pop", "[PTP]") {
 	stack<int> A;
 	A.push(4);
+	A.push(404);
+	REQUIRE(A.count() == 2);
+	REQUIRE(A.top() == 404);
+	A.pop();
 	REQUIRE(A.count() == 1);
 	REQUIRE(A.top() == 4);
 	A.pop();
@@ -20,18 +24,4 @@ SCENARIO("operator =","[=]"){
 	stack<int> B = A; 
 	REQUIRE(B.count() == 1);
 	REQUIRE(B.top() == 221);
-}
-SCENARIO("Stack push, top and pop #2", "[PTP#2]") {
-	stack<int> A;
-	int m=404;
-	for (int i=0; i<10; ++i){
-		A.push(m+i);
-	}
-	bool A=True;
-	for (int i=0; i<10; ++i){
-		if (A.count()!=10-i) { A=False;}
-		if (A.top!=m-i) { A=False; }
-		A.pop;
-	}
-	REQUIRE(A==True);
 }
