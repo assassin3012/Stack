008d4a90f7db33932115d32c214c6f495f2e34c6
assassin3012 <assassin3012@mail.ru>
2016-09-26 01:35:02 +0300

    Update stack.h

--- test/stack.h
+++ test/stack.h
@@ -6,19 +6,19 @@ template <typename T>
 class stack
 {
 public:
-	stack();
-	stack(const stack<T> & st);
-	~stack();
-	size_t count() const noexcept;
-	void push(T const & el);
-	stack<T> & operator = (stack<T> & st);
-	void pop();
-	T& top() const;
+	stack(); /*noexcept*/
+	stack(const stack<T> & st); /*strong*/
+	~stack(); /*noexcept*/
+	size_t count() const noexcept; /*noexcept*/
+	void push(T const & el); /*strong*/
+	stack<T> & operator = (stack<T> & st); /*noexcept*/
+	void pop(); /*strong*/
+	T& top() const; /*strong*/
 private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
-	void swap(stack & st);
-};
+	void swap(stack & st); /*noexcept*/
+
 #include "stack.cpp"
 #endif
