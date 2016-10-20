0f07956e16025d43360abc0424df84a6aad69a3d
assassin3012 <assassin3012@mail.ru>
2016-10-21 01:04:20 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -31,10 +31,6 @@ public:
 	stack<T> & operator = (stack<T> & st); /*strong*/
 	void pop(); /*strong*/
 	T& top() const; /*strong*/
-private:
-	T * array_;
-	size_t array_size_;
-	size_t count_;
 	void swap(stack & st); /*noexcept*/
 };
 #include "stack.cpp"
