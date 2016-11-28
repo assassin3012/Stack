36a41a37a06050035ce7178f072d7195666adb85
Lair Sinh <assassin3012@mail.ru>
2016-11-29 00:25:46 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,18 +2,6 @@
 #include <iostream>
 #include <catch.hpp>
 
-SCENARIO("Stack threads", "threads") {
-	stack<int> A;
-	A.push(13);
-	A.push(666);
-	std::thread t1(&stack<int>::push, &A, 404);
-	t1.join();
-	REQUIRE(A.count() == 3);
-	std::thread t2(&stack<int>::pop, &A);
-	t2.join();
-	REQUIRE(*A.pop() == 666);
-	REQUIRE(A.count() == 1);
-}
 SCENARIO("Stack init, count and empty", "[init_count]"){
 	stack<bool> A;
 	REQUIRE(A.count() == 0);
@@ -24,9 +12,11 @@ SCENARIO("Stack push, top and pop", "[PTP]") {
 	A.push(4);
 	A.push(404);
 	REQUIRE(A.count() == 2);
-	REQUIRE(*A.pop() == 404);
+	REQUIRE(A.top() == 404);
+	A.pop();
 	REQUIRE(A.count() == 1);
-	REQUIRE(*A.pop() == 4);
+	REQUIRE(A.top() == 4);
+	A.pop();
 	REQUIRE(A.empty());
 }
 SCENARIO("Stack operator =","[operator =]"){
@@ -34,11 +24,11 @@ SCENARIO("Stack operator =","[operator =]"){
 	A.push(221);
 	stack<int> B = A; 
 	REQUIRE(B.count() == 1);
-	REQUIRE(*B.pop() == 221);
+	REQUIRE(B.top() == 221);
 	stack<int> A1;
 	stack<int> B1;
 	A1.push(221);
 	B1 = A1; 
 	REQUIRE(B1.count() == 1);
-	REQUIRE(*B1.pop() == 221);
+	REQUIRE(B1.top() == 221);
 }
